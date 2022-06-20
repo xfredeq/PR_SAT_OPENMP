8e681f227e5eed99be5a41a66bd95711b46902b5
xfredeq-ubuntu <65500997+xfredeq@users.noreply.github.com>
2022-06-20 20:13:07 +0200

    add file parameter

--- main.cpp
+++ main.cpp
@@ -5,15 +5,29 @@
 bool debug = false;
 
 int main(int argc, char *argv[]) {
+    omp_set_dynamic(0);
+    std::string fileName = "inputfile.txt";
+    int threads = 4;
 
-    if (argc > 1) {
-        int threads = std::stoi(argv[1]);
-        omp_set_num_threads(threads);
-    } else {
-        omp_set_num_threads(4);
+    if (argc > 2) {
+        try {
+            fileName = argv[1];
+            threads = std::stoi(argv[2]);
+        } catch (const std::exception &e) {
+            printf("First argument - file name, second - number of threads.");
+        }
+    } else if (argc > 1) {
+        try {
+            fileName = argv[1];
+        } catch (const std::exception &e) {
+            printf("First argument - file name, second - number of threads.");
+        }
     }
+
+    omp_set_num_threads(threads);
+
     SATSolver solver(debug);
-    if (!solver.loadFromFile("inputfile.txt")) {
+    if (!solver.loadFromFile(fileName)) {
         return 1;
     }
     solver.findResult();
